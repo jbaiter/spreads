f2e46409f844e3aac7254d004d90f7aca77b2370
Johannes Baiter <johannes.baiter@gmail.com>
2015-04-07 10:32:07 +0200

    web-client: Add AGPLv3 header to all source files

--- spreadsplug/web/client/src/utils/WebAPIUtils.js
+++ spreadsplug/web/client/src/utils/WebAPIUtils.js
@@ -1,3 +1,23 @@
+/*
+ * Spreads - Modular workflow assistant for book digitization
+ * Copyright (C) 2013-2015 Johannes Baiter <johannes.baiter@gmail.com>
+ *
+ * This file is part of Spreads.
+ *
+ * Spreads is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Spreads is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Spreads.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import map from "lodash/collection/map";
 
 export function makeUrl(...parts) {
