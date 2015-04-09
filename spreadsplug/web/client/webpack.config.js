768649cd9e661e3488b494a3b109d452e4d98453
Johannes Baiter <johannes.baiter@gmail.com>
2015-04-09 02:44:34 +0200

    web-client: Add some React optimizations to webpack config

--- spreadsplug/web/client/webpack.config.js
+++ spreadsplug/web/client/webpack.config.js
@@ -25,6 +25,16 @@ if (!__DEV__) {
 }
 plugins.push(definePlugin);
 
+var babelOpts = {
+  optional: ["runtime"]
+}
+
+var babelJsxOpts = {
+  optional: ["runtime", "optimisation.react.constantElements"]
+}
+if (!__DEV__) {
+  babelJsxOpts.optional.push("optimisation.react.inlineElements");
+}
 module.exports = {
   cache: true,
   debug: __DEV__,
@@ -38,9 +48,18 @@ module.exports = {
   module: {
     loaders: [
       {test: require.resolve("react"), loader: "expose?React"},
-      {test: /\.js$/, loaders: ["babel-loader?optional=runtime", "eslint-loader"], exclude: /node_modules/},
-      {test: /\.jsx$/, loaders: ["babel-loader?optional=runtime", "eslint-loader"], exclude: /node_modules/},
-      {test: /\.jsx$/, loaders: ["babel-loader?optional=runtime"], exclude: /client\/src/},
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader?" + JSON.stringify(babelOpts), "eslint-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        loaders: ["babel-loader?" + JSON.stringify(babelJsxOpts), "eslint-loader"],
+        exclude: /node_modules/
+      },
+      {test: /\.jsx$/, loaders: ["babel-loader?" + JSON.stringify(babelJsxOpts)],
+       exclude: /client\/src/},
       {test: /\.css$/, loader: "style!css"},
       {test: /\.scss$/, loader: "style!css!sass"},
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
